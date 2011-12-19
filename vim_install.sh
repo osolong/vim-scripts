65259e45fad8050ecdd76dbdce2e4e5a73377d6f
brlrrodr <brlrrodr@brlrrodr-Inspiron.(none)>
2011-12-19 11:25:35 -0600

    Fixed typo

--- vim_install.sh
+++ vim_install.sh
@@ -10,7 +10,7 @@ WGET_PROJECT=http://www.vim.org/scripts/download_script.php?src_id=6273
 WGET_ENH_COMMENT=http://www.vim.org/scripts/download_script.php?src_id=8319
 WGET_SUPER_TAB=http://www.vim.org/scripts/download_script.php?src_id=16104i
 WGET_CSCOPE_MAPS=http://cscope.sourceforge.net/cscope_maps.vim
-=
+
 
 initial_setup () {
     echo "========================================================"
